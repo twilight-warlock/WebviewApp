bf46fdb5af262c2b81ade48d6f7eecf59f54e728
Vikas <vikas.rg@somaiya.edu>
2021-02-22 13:42:57 +0530

    Formatting

--- App.js
+++ App.js
@@ -1,184 +1,133 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, { Component } from 'react'
-
-import {View} from "react-native"
-
+import { View } from "react-native"
 import { WebView } from 'react-native-webview';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import List from "./Components/List"
 
-const storageSchema = {
-  primary:0,
-  data:[
-    {
-      name:"Google",
-      link:"www.google.com",
-      userName:"abc",
-      password:"def"
-    },
-    {
-      name:"Facebook",
-      link:"www.facebook.com",
-      userName:"abc",
-      password:"def"
-    },
-    {
-        name:"Yahoo",
-        link:"www.yahoo.com",
-        userName:"abc",
-        password:"def"
-    },
-    {
-        name:"Stack OverFlow",
-        link:"https://stackoverflow.com/",
-        userName:"abc",
-        password:"def"
-    }
-  ]
-}
-
-
-
-
 class App extends Component {
-  constructor(props){
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state={
-      currentUrl:"https://www.google.com/",
-      storage: {
-        primary:0,
-        data:[
-          {
-            name:"Google",
-            link:"www.google.com",
-            userName:"abc",
-            password:"def"
-          },
-        ]
-      },
-      delConfirm:false
-    }
-    this.updateUrl = this.updateUrl.bind(this)
-    this.updatePrimary = this.updatePrimary.bind(this)
-    this.storeData = this.storeData.bind(this)
-    this.confirmDel = this.confirmDel.bind(this)
-    this.delData = this.delData.bind(this)
-  }
+		this.state = {
+			currentUrl: null,
+			storage: {
+				primary: 0,
+				data: []
+			},
+			delConfirm: false
+		}
+	}
 
-  componentDidMount(){
-    this.storeData(storageSchema);
-    this.getData()
-  }
+	componentDidMount() {
+		this.getData();
+	}
 
-  updateUrl(url){
-    this.setState({currentUrl:url})
-  }
-  updatePrimary = async(index) => {
-    this.getData();
-    let temp = this.state.storage;
-    console.log(index);
-    temp["primary"] = index;
-    // console.log(temp);
-    this.storeData(temp);
-    this.setState({storage:temp})
-    console.log(this.state);
-  }
+	updateUrl(url) {
+		this.setState({ currentUrl: url })
+	}
 
-  storeData = async (value) => {
-    try {
-      const jsonValue = JSON.stringify(value)
-      const value = await AsyncStorage.setItem('storage', jsonValue)
-    } catch (e) {
-      console.log(e);
-    }
-  }
+	updatePrimary = async (index) => {
+		this.getData();
+		let temp = this.state.storage;
+		console.log(index);
+		temp["primary"] = index;
+		// console.log(temp);
+		this.storeData(temp);
+		this.setState({ storage: temp })
+		console.log(this.state);
+	}
 
-  getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem('storage')
-      if(value !== null) {
-        // console.log(JSON.parse(value));
-        this.setState({storage:JSON.parse(value)})
-      }
-    } catch(e) {
-      console.log(e);
-    }
-  }
-  
-  getValue = async (obj) => {
-    try {
-      let value = await AsyncStorage.getItem('storage')
-      if(value !== null) {
-        let newdata = JSON.parse(value)["data"]
-        newdata.push(obj)
-        console.log(newdata);
-        let newValue = JSON.parse(value);
-        newValue["data"] = newdata;
-        this.storeData(newValue)
-        this.setState({storage:newValue})
-      }
-    } catch(e) {
-      console.log(e);
-    }
-  }
+	storeData = async (value) => {
+		try {
+			const jsonValue = JSON.stringify(value)
+			await AsyncStorage.setItem('storage', jsonValue)
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  confirmDel(){
-    this.setState({delConfirm:true});
-    console.log(this.state.delConfirm);
-  }
+	getData = async () => {
+		try {
+			const value = await AsyncStorage.getItem('storage')
+			if (value !== null) {
+				this.setState({ storage: JSON.parse(value) })
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  delData(index){
-    console.log(index);
-    if(this.state.delConfirm){
-        console.log("success");
-        // this.delAndUpdate();
-        this.setState({delConfirm:false})
-    }
-  }
+	getValue = async (obj) => {
+		try {
+			let value = await AsyncStorage.getItem('storage')
+			if (value !== null) {
+				let newdata = JSON.parse(value)["data"]
+				newdata.push(obj)
+				console.log(newdata);
+				let newValue = JSON.parse(value);
+				newValue["data"] = newdata;
+				this.storeData(newValue)
+				this.setState({ storage: newValue })
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  delAndUpdate = async (index) => {
-    try {
-      let value = await AsyncStorage.getItem('storage')
-      if(value !== null) {
-        let newdata = JSON.parse(value)["data"]
-        newdata.splice(index,1)
-        console.log(newdata);
-        let newValue = JSON.parse(value);
-        newValue["data"] = newdata;
-        this.storeData(newValue)
-        this.setState({storage:newValue})
-      }
-    } catch(e) {
-      console.log(e);
-    }
-  }
+	confirmDel = () => {
+		this.setState({ delConfirm: true });
+		console.log(this.state.delConfirm);
+	}
 
-  render() {
-    return (
-      <>
-        <View style={{flex:1}}>
-          <WebView source={{ uri: this.state.currentUrl }} />
-          <List 
-            update={this.updateUrl} 
-            storage={this.state.storage} 
-            updatePrimary={this.updatePrimary} 
-            getValue={this.getValue} 
-            confirmDel={this.confirmDel} 
-            delData={this.delData} 
-            
-          />
-        </View>
-      </>
-    )
-  }
+	delData = (index) => {
+		if (this.state.delConfirm) {
+			console.log("success");
+			// this.delAndUpdate();
+			this.setState({ delConfirm: false })
+		}
+	}
+
+	delAndUpdate = async (index) => {
+		try {
+			let value = await AsyncStorage.getItem('storage')
+			if (value !== null) {
+				let newdata = JSON.parse(value)["data"]
+				newdata.splice(index, 1)
+				console.log(newdata);
+				let newValue = JSON.parse(value);
+				newValue["data"] = newdata;
+				this.storeData(newValue)
+				this.setState({ storage: newValue })
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	render() {
+		return (
+			<>
+				<View style={{ flex: 1 }}>
+					{this.state.currentUrl ?
+						<WebView source={{ uri: this.state.currentUrl }} />
+						:
+						<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+							
+						</View>
+					}
+					<List
+						update={this.updateUrl}
+						storage={this.state.storage}
+						updatePrimary={this.updatePrimary}
+						getValue={this.getValue}
+						confirmDel={this.confirmDel}
+						delData={this.delData}
+
+					/>
+				</View>
+			</>
+		)
+	}
 }
 
 export default App;
